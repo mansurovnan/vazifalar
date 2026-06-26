8add148d55f476e8e65a5abfc16a650437be55c3
mansurovnan <nozaninorolova2@gmail.com>
2026-06-26 20:08:04 +0500

    users

---
+++ hometask4/products2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Products2Config(AppConfig):
+    name = 'products2'
--- hometasks/hometask2/vazofa2/vazifa2.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-
-def test(request):
-    return HttpResponse('sen uddalading')
-
-def home(request):
-    return HttpResponse('ma sha alloh sen yana uddalading')
