36f7b6a432ffcb46e8e7a59163406bb24a57f120
mansurovnan <nozaninorolova2@gmail.com>
2026-06-26 21:18:50 +0500

    done

--- hometask1/names/views.py
+++ hometask1/names/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def get_users(request):
+    return HttpResponse('malika, odina')
+def get_best_users(request):
+    return HttpResponse('umar')
--- hometask1/vazifa1/urls.py
+++ hometask1/vazifa1/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .vazifa1 import test, home
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', test),
-    path('index', home)
-]
-
-from django.contrib import admin
-from django.urls import path
+from names.views import get_users, get_best_users
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('names', get_users),
+    path('best_users', get_best_users)
+    
 ]
+
+
